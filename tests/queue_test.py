e16eca82b4357f158a468b63f9699b544c8c9286
futo <fedor.alekseev.msk@gmail.com>
2026-04-26 16:28:13 +0300

    Update for new queue and async implementation

--- tests/queue_test.py
+++ tests/queue_test.py
@@ -1,36 +1,30 @@
-from pytest import raises, mark
-from random import randint, choice, seed
+from pytest import mark
+from random import randint, seed
 from src.task import Task
 from src.queue import TaskQueue
-from src.exceptions import TaskError
 from src.source import RandomSource
-
 
 @mark.asyncio
 async def test_task_queue():
     seed() # Сид генерации
-    amount = randint(10,500)
+    amount = randint(10, 100)
     source = RandomSource(amount)
     tasks = await source.get_tasks()
 
     collection = TaskQueue()
     for task in tasks:
-        collection.add_task(task)
-
+        task.task_type = "default"
+        await collection.add(task)
     i = 0
     for task in collection:
         assert task == tasks[i]
         i += 1
     assert amount == i
-    collection.delete(choice(list(collection._tasks.values())))
+    if tasks:
+        target_prio = tasks[0].priority
+        filtered = list(collection.filter_by_priority(target_prio))
+        assert len(filtered) > 0
+        assert all(t.priority == target_prio for t in filtered)
+    first_task = await collection.get()
+    assert first_task == tasks[0]
     assert len(list(collection)) == amount - 1
-
-    non_existing_task = Task("task_-1", "description", 1)
-    with raises(TaskError):
-        collection.delete(non_existing_task)
-
-    assert collection.find(non_existing_task) is None
-    some_task = choice(list(collection._tasks.values()))
-    assert collection.find(some_task.id) == some_task
-    with raises(TaskError):
-        collection.add_task(some_task)
