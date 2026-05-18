afa3990d0e3267b98a03c4511cb3a4b137e93539
futo <fedor.alekseev.msk@gmail.com>
2026-05-18 18:50:26 +0300

    Sync queue iterator with previous lab

--- src/queue.py
+++ src/queue.py
@@ -5,13 +5,29 @@ from collections import deque
 from typing import Deque
 
 
+class TaskIterator:
+    def __init__(self, tasks: Deque[Task]):
+        self._tasks = list(tasks)
+        self._index = 0
+
+    def __next__(self) -> Task:
+        if self._index >= len(self._tasks):
+            raise StopIteration
+        current_task = self._tasks[self._index]
+        self._index += 1
+        return current_task
+
+    def __iter__(self):
+        return self
+
+
 class TaskQueue:
     def __init__(self):
         self._queue: Deque[Task] = deque()
         self._condition = asyncio.Condition()
 
     def __iter__(self):
-        return iter(list(self._queue))
+        return TaskIterator(self._queue)
 
     async def add(self, task: Task):
         async with self._condition:
